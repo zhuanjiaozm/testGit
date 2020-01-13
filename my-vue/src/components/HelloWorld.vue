168428d394cb2f5a639ee56611d35d22e0990f30
乔均 <602763799@qq.com>
2020-01-13 16:24:34 +0800

    提交

---
+++ my-vue/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="dragbox">
+		<p>我是来测试的</p>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		},
+		mounted() {
+			
+		}
+	}
+</script>
+
+<style scoped>
+	
+</style>
